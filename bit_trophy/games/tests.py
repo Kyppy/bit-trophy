29d20230928fed281052e8b701938fee9ff180b2
Kyppy Ngaira Simani <simanikyppy@gmail.com>
2019-03-13 10:03:14 +0300

    Travis settings initialised

--- bit_trophy/games/tests.py
+++ bit_trophy/games/tests.py
@@ -3,7 +3,7 @@ from .models import VideoGame
 
 
 class RetrieveGameTestcase(TestCase):
-    """This class defines the test suite for the videogame model."""
+    """This class defines the test suite for the 'VideoGame' model."""
 
     def setUp(self):
         """Define the test client and other test variables."""
@@ -11,7 +11,7 @@ class RetrieveGameTestcase(TestCase):
         self.videogame = VideoGame(title=self.videogame_title)
 
     def test_model_can_create_a_bucketlist(self):
-        """Test that the videogame model can create a videogame instance."""
+        """Test that the VideoGame model can create a single instance."""
         old_count = VideoGame.objects.count()
         self.videogame.save()
         new_count = VideoGame.objects.count()
