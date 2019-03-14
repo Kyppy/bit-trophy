8f1634d25fce0f00df3768cdd79f2ff78b724828
Kyppy Ngaira Simani <simanikyppy@gmail.com>
2019-03-14 20:55:13 +0300

    Created new view to post an entry to database

--- bit_trophy/game/views.py
+++ bit_trophy/game/views.py
@@ -13,3 +13,12 @@ class GamesView(APIView):
         games = VideoGame.objects.all()
         serializer = GameSerializer(games, many=True)
         return Response({"games": serializer.data})
+    
+    """Creates a game post using data from the request"""
+    def post(self, request):
+        game = request.data.get('game')
+        serializer = GameSerializer(data=game)
+        if serializer.is_valid(raise_exception=True):
+            save_game = serializer.save()
+        return Response({"success": "Your '{}' post was successful"
+                        .format(save_game.title)})
