3f32fab7fc564e7e85d5d1ef2948f6766e1a7db7
Kyppy Ngaira Simani <simanikyppy@gmail.com>
2019-03-15 16:13:44 +0300

    Add new route for PATCH view

--- bit_trophy/game/urls.py
+++ bit_trophy/game/urls.py
@@ -8,4 +8,5 @@ app_name = "game"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
     path('games/', GamesView.as_view()),
+    path('games/<int:pk>', GamesView.as_view()),
 ]
