b92e767afeac53999e1e857d3285f4deb5bd6969
Kyppy Ngaira Simani <simanikyppy@gmail.com>
2019-03-18 16:52:23 +0300

    Heroku debug

--- bit_trophy/bit_trophy/settings.py
+++ bit_trophy/bit_trophy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b2_lzd=!*uqgo_bq2y_(cyt63!ijxcvm#nb+^s5)43(u0(1=b('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bit-trophy.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
